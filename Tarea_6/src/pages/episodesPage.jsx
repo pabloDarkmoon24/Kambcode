6c3c6accc738f3620c7fa2fe523588cc8c56dfb8
Pablo24 <vtrpablo4@gmail.com>
2024-09-15 22:47:00 -0500

    Tarea_6,Crear una aplicación de una sola página con múltiples rutas

---
+++ Tarea_6/src/pages/episodesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EpisodesRickMorty from '../components/episodesRickMorty/episodesRickMorty'
+
+const EpisodesPage = () => {
+  return (
+    <div>
+        <EpisodesRickMorty/>
+    </div>
+  )
+}
+
+export default EpisodesPage
